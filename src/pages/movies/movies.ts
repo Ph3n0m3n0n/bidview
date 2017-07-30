cbb2fa621514cfbc3a8bc0957af8b200fefa2b50
Erza Scarlet <Erza Scarlet>
2017-07-29 21:11:53 -0500

    added CRUD

--- src/pages/movies/movies.ts
+++ src/pages/movies/movies.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
 import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 
 @Component({
@@ -10,8 +10,103 @@ export class MoviesPage {
 
   movies: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController, aDB: AngularFireDatabase) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, aDB: AngularFireDatabase, public actionSheetCtrl: ActionSheetController) {
     this.movies = aDB.list('/movies');
   }
+
+  addMovie(){
+    let prompt = this.alertCtrl.create({
+      title: 'Movie Title',
+      message: 'Enter a movie title',
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel button clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.movies.push({
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  showOptions(movieId, movieTitle) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'What do you want to do?',
+      buttons: [
+        {
+          text: 'Delete Movie',
+          role: 'destructive',
+          handler: () => {
+            this.removeMovie(movieId);
+          }
+        }, {
+          text: 'Update title',
+          handler: () => {
+            this.updateMovie(movieId, movieTitle);
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+              console.log('Cancelled');
+          }
+        }
+      ]
+
+    });
+    actionSheet.present();
+  }
+
+      removeMovie(movieId) {
+        this.movies.remove(movieId);
+      }
+
+
+updateMovie(movieId, movieTitle) {
+  let prompt = this.alertCtrl.create({
+    title: 'Movie Title',
+    message: 'Update Movie Title',
+    inputs: [
+      {
+        name: 'title',
+        placeholder: 'Title',
+        value: movieTitle
+      },
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+        handler: data => {
+          console.log('Cancelled');
+        }
+      },
+      {
+        text: 'Save',
+        handler: data => {
+          this.movies.update(movieId, {
+            title: data.title
+          });
+        }
+      }
+    ]
+  });
+  prompt.present();
+}
+
 
 }
